01cd393d6c36ae2b2c2240bc2da57797364e185b
Shobha Singh <ssingh7@yahoo.com>
2026-01-10 21:35:28 -0500

    commented out

--- src/pages/About.js
+++ src/pages/About.js
@@ -28,9 +28,7 @@ const About = () => {
             <h2>
               <Link to="/about">About Me</Link>
             </h2>
-            <!--
-            <p>(in about {count} words)</p>
-            -->
+            {/* <p>(in about {count} words)</p> */}
           </div>
         </header>
         <Markdown>{markdown}</Markdown>
